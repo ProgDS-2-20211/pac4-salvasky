aa784b0a9e64e0468023c88cbf31c2848caba411
salvasky <salva.sanchis@me.com>
2021-12-27 17:33:27 +0100

    Folder structure set up

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='trax',
+    version='',
+    packages=['trax'],
+    url='',
+    license='',
+    author='salvador sanchis',
+    author_email='ssanchisb@uoc.edu',
+    description='music data analysis'
+)
