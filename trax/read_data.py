26e4acd1379157cf28b1411d09e1072dc08daa4b
salvasky <salva.sanchis@me.com>
2021-12-28 07:42:10 +0100

    Extract only csv files from zip

--- trax/read_data.py
+++ trax/read_data.py
@@ -1,4 +1,5 @@
 import zipfile as zf
+import os
 import pandas as pd
 
 def import_zip(path):
@@ -7,12 +8,12 @@ def import_zip(path):
     :rtype: info
     """
     with zf.ZipFile(path, 'r') as zip_f:
-       # Get a list of all archived file names from the zip
-       file_list = zip_f.namelist()
-       print(zip_f.printdir())
-       # Iterate over the file names
-       for fileName in file_list:
-           # Check filename endswith csv
-           if fileName.endswith('.csv'):
-               # Extract a single file from zip
-               zip_f.extract(fileName, 'csv_files')
+        print(zip_f.printdir())
+        #file_list = zip_f.namelist()
+        top = {item.split('/')[0] for item in zip_f.namelist()}
+        # Iterate over the file names
+        for fileName in top:
+            # Check filename endswith csv
+            if fileName.endswith('.csv'):
+                # Extract a single file from zip
+                zip_f.extract(fileName, 'csv_files')
