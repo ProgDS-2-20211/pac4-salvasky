3d08f3e09a1e0af6badaa1fb527978c114b11adf
salvasky <salva.sanchis@me.com>
2021-12-28 08:19:12 +0100

    Capitalized artists names

--- task_1.py
+++ task_1.py
@@ -4,4 +4,6 @@ from trax import read_data as rd
 
 rd.import_zip('data/data.zip')
 
+artists = pd.read_csv('csv_files/artists_norm.csv', sep=';', converters={"name": lambda x: x.title()})
 
+print(artists.to_string())
