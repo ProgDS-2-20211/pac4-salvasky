64c0813d2704a86b78fc9ec54097da331acc9269
salvasky <salva.sanchis@me.com>
2021-12-28 19:14:30 +0100

    added comments and docstrings to task 1

--- task_1.py
+++ task_1.py
@@ -1,22 +1,29 @@
+# Importem llibreries
 import pandas as pd
-
 from trax import read_data as rd
 
+# Executem el mòdul per descomprimir els fitxers:
 rd.import_zip('data/data.zip')
 
+# Transformem els fitxers a dataframes:
 artists = pd.read_csv('csv_files/artists_norm.csv', sep=';', converters={"name": lambda x: x.title()})
 tracks = pd.read_csv('csv_files/tracks_norm.csv', sep=';')
 albums = pd.read_csv('csv_files/albums_norm.csv', sep=';')
 
+# Mostrem els criteris d'acceptació
 print("The dataset contains {} tracks".format(rd.count_tracks(tracks)))
 print("There were {} tracks in the set that had no popularity score".format(tracks['popularity'].isna().sum()))
 
-tracks['popularity'] = tracks['popularity'].fillna(tracks['popularity'].mean())
+# Imputem els valors buits del dataset tracks:
+rd.impute(tracks, 'popularity')
 
+# Unim els datasets tracks i albums:
 left_merge = pd.merge(tracks, albums, how='left', on=['album_id', 'artist_id'], suffixes=("_track", "_album"))
 
+# Hi afegim el dataset artists:
 all_merge = pd.merge(left_merge, artists, how='left', on='artist_id', suffixes=(" ", '_artist'))
-all_merge.rename(columns = {'name':'artist_name', 'popularity':'artist_popularity','followers':'artist_followers',
-                            'total_albums':'artist_total_albums'}, inplace = True)
+all_merge.rename(columns = {'total_tracks':'album_total_tracks', 'name':'artist_name', 'popularity':'artist_popularity',
+                            'followers':'artist_followers', 'total_albums':'artist_total_albums'}, inplace = True)
 
+# Exportem el nou fitxer desnormalitzat a csv:
 all_merge.to_csv(path_or_buf='data/tracks.csv', sep=';', index=False)
--- trax/read_data.py
+++ trax/read_data.py
@@ -1,11 +1,13 @@
 import zipfile as zf
-import os
-import pandas as pd
 
 def import_zip(path):
     """
-
-    :rtype: info
+    Decompresses zip files
+    only from top level of directory,
+    skipping subdirectories.
+    Creates a new folder to host
+    the files.
+    :param path: location of the zip folder
     """
     with zf.ZipFile(path, 'r') as zip_f:
         top = {item.split('/')[0] for item in zip_f.namelist()}
@@ -17,7 +19,19 @@ def import_zip(path):
                 zip_f.extract(fileName, 'csv_files')
 
 def count_tracks(df):
+    """
+    Counts number of entries in dataset
+    :param df: name of dataset
+    :return: int
+    """
     index_df = df.index
     number_of_tracks = len(index_df)
     return number_of_tracks
 
+def impute(dataframe, column):
+    """
+    Imputes empty values using column mean value
+    :param dataframe: dataset to impute
+    :param column: column name to impute
+    """
+    dataframe[column] = dataframe[column].fillna(dataframe[column].mean())
